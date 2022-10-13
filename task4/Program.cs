5ced2a8062311ada1b15ae6916b7bfc3264d9132
Alyona <nakamura-san@mail.ru>
2022-10-13 19:18:44 +0300

    Task 4

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,19 +2,22 @@
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Console.Clear();
 
-    void InputMatrix(int[,,] matrix)
-{
+    void InputMatrix(int[,,] matrix) 
+{ 
+        int count = 10;
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
-                for (int k = 0; k < matrix.GetLength(2); k++)
+                for (int k = 0; k < matrix.GetLength(2); k++)    
                 {  
-                        matrix[i, j, k] = new Random().Next(10, 100);
+                        matrix[i, j, k] = count;
+                        count++;
                 }
             }
         }
-}
+     }
+
 void PrintMatrix(int[,,] matrix)
     {
         Console.WriteLine();
